029a65c345602affd41c9ae2ad259fed263a672e
Keanu Ventura <keventura@ucsd.edu>
2025-10-20 20:59:10 -0700

    stuff

--- global.js
+++ global.js
@@ -65,3 +65,18 @@ form?.addEventListener('submit', (event) => {
     const url = `${form.action}?${params.join('&')}`;
     location.href = url;
 });
+export async function fetchJSON(url) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      const data = await response.json();
+        return data;
+    } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+    }
+  }
+  export function renderProjects(project, containerElement) {
+    // Your code will go here
+  }
